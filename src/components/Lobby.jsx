71b6f4d8926f1e72cada37e8ab8301f9b1a24b4d
vmmuthu31 <mvairamuthu2003@gmail.com>
2023-12-07 01:36:14 +0530

    updated the images in drive

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -20,11 +20,11 @@ function Lobby() {
 
   return (
     <>
-      <div className="bg-[url('../../public/assets/homebg.png')] min-h-screen  bg-no-repeat bg-cover">
+      <div className="bg-[url('https://blogger.googleusercontent.com/img/a/AVvXsEhnD6k83f7VLrtmGoYKcqJk_lSnWZwgDl-Fgz3RTsKgatlb276B0QhB3xi_mEl7jsRIVgUxASTVenCDoqXju-Q2YfCi__LT70OcLJjYgbl8AE1tcqPdHGYCUgzUuUI5DN5J9hQBHSVyb9AA_BZSwfaZXL-RxvvIRtqT3P0jsV1YzD_rb62S9reTOCxg24k')] min-h-screen  bg-no-repeat bg-cover">
         <div className="flex items-center space-x-11 text-white text-xl mx-20 py-8">
           <div className="flex homeprofilebg px-3 py-2 items-center space-x-3">
             <img
-              src="../../public/assets/profile.png"
+              src="https://blogger.googleusercontent.com/img/a/AVvXsEilxD0f-Y5qYnr3AA8xT_tvMlR7ru7Yl1zxozlEzg-C5oJqOStwAR8OxsgItoWC112TQTgCt4_xylJDmr4v_Z_A3MDUy22L6CAI_Cvw_FnicYCcoXScwCt41T-xiWNZ8JQJyfbXNdygsgY9TxXvH-Yqdg0vqpeMrakh78RxXj5BAT4XwW1a3KsQVhexzog"
               className="h-12 w-auto"
               alt=""
             />
@@ -32,7 +32,7 @@ function Lobby() {
           </div>
           <div className="flex items-center space-x-2">
             <img
-              src="../../public/assets/coin.png"
+              src="https://blogger.googleusercontent.com/img/a/AVvXsEispplhVXS52zWgstszpWTDQTrJ7FpVpnN4YjBilPRJ0hmtf0FGRI1-JoXko1x1mIG4Gi7ADUF3Yl9lu5JlsLRFnGUcPJnJzStlHom3K63Wu2QcL-nsJoMq2V66FcenoK7MbQVn_9vg1_8E1Q25wDoQJb2AGKiq4JGDYyknSKoXzYQFFR8LEhpX-R13ad4"
               alt=""
               className="h-8 w-auto"
             />
@@ -40,7 +40,7 @@ function Lobby() {
           </div>
           <div className="flex items-center space-x-2">
             <img
-              src="../../public/assets/diamond.png"
+              src="https://blogger.googleusercontent.com/img/a/AVvXsEie2DZwyszxtLdkqYknRhqV0hDa85fb4knhn16GCCa3HO6AB_BHA19-BnWKl5qzuE8oOJ_WVifNg1FdY05UTucSiz36llzpSqUBjYbOriIDtaQV9iLJe0eMs455RVi3wkImTId7l0BqdOamXFulz7jivdeEiXqlhfItGYU-7iDuUgSBWA1PweMDY341yFM"
               alt=""
               className="h-8 w-auto"
             />
@@ -52,7 +52,7 @@ function Lobby() {
             <div className="homebox mt-20 px-16 py-10">
               <div className="flex items-center space-x-5">
                 <img
-                  src="../../public/assets/shop.png"
+                  src="https://blogger.googleusercontent.com/img/a/AVvXsEhwze50sr7c42qWHWl1ZtWP-h91tRw96mnDxbST2rhMGENwxAH4LRxTWod417CEaB4xQfPVZ-0-kB1XCD2BDn1hwqxTPxNK6Z_Dz8F7Fo8hDjazJX_zXr458VZUPjdzdih1xheqz4yJg7oXTEQizG8q-8vC2B69RhKN4WOO6XS0AvvMhgGSGkq64aSJ3dQ"
                   alt=""
                   className="h-8 w-auto"
                 />
@@ -62,7 +62,7 @@ function Lobby() {
               </div>
               <div className="flex items-center my-5 space-x-5">
                 <img
-                  src="../../public/assets/award.png"
+                  src="https://blogger.googleusercontent.com/img/a/AVvXsEgn6Znvl2a2HObGhEoqPyeJymSTwEqIxV8f7IIQK3sCnu7oyYtZCkSg4XB-SRkV7NaxN7OVjliWj7gsOcc9VFmULUPaex4K3A1oEWf6wNsLfa8y9CcwLEdA52Dh-Hl2OnevhWJVJlI7CAMUpnWT97KEO42TfPhAxgHi7umyV4vGcVoO_XTnyxpNyJasnPg"
                   alt=""
                   className="h-8 w-auto"
                 />
@@ -72,7 +72,7 @@ function Lobby() {
               </div>
               <div className="flex items-center space-x-5">
                 <img
-                  src="../../public/assets/packagebox.png"
+                  src="https://blogger.googleusercontent.com/img/a/AVvXsEj9mP_S5zrE05iA7nZDHHKPCR4xSdtSRPtzr9tu1TMRYbTkG9wNiCq_Ri20Nna07x-B775iuyjcJBplvhELJglNv426Q-hq-SVkXOhxSDrBLoROEbIAxMzxcUSWOaNF5lpgFBf35PUWkcEoyFN-rhZnwh9o4Q8ply2YLZrxTbmzr_zobAF7jEPIIunNH9s"
                   alt=""
                   className="h-8 w-auto"
                 />
@@ -92,7 +92,7 @@ function Lobby() {
           <div className="mt-56 ">
             <div className="flex mapbox px-6 py-3 items-center space-x-5">
               <img
-                src="../../public/assets/packagebox.png"
+                src="https://blogger.googleusercontent.com/img/a/AVvXsEhJ85zCrUSpRV7cSOt5Y1VeibTq8106ipzp_Ow_LZxxFvl2BDdUTpR0N5LVWnfhcA8DjymoCzOOgAl_3P4kpI9QXB2MJBEm6DP1n6kbleCpf_8IY_uaucIZpKyAwZjNJd9XzG2GRbyyqMhX5FKrNeKg1UAj0WLoxEA8b9hKg-eXqJi7IralLJYl8fnj2Uk"
                 alt=""
                 className="h-8 w-auto"
               />
@@ -100,7 +100,7 @@ function Lobby() {
             </div>
             <div className="">
               <img
-                src="../../public/assets/homedop.png"
+                src="https://blogger.googleusercontent.com/img/a/AVvXsEhLX30N2aeEQt2Iv9xNfAUagS2HOylDbudaDn_uyM3NvtggvfRsS1vtFPFNdsTGoXFrloA-dYaXtJDkztDAc-hxIzsHhI_XcZycIV0Az6fTR8jxGWl2lgd8PQPpLaoZ3KWgTubfv9-x2RfEyqM7mmBq3SoytIX0AH8jKNFgKEw7UeENmC4fERXuwnaA9JA"
                 className="h-40 w-auto"
                 alt=""
               />
